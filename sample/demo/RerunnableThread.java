dbfedcb44db8d1e99864c753eb29825cddef07c2
ut-aki <39979390+ut-aki@users.noreply.github.com>
2018-06-08 20:27:26 +0900

    Create RerunnableThread.java

---
+++ sample/demo/RerunnableThread.java
@@ -0,0 +1,54 @@
+package demo;
+
+import java.lang.Thread;
+import java.util.ArrayList;
+
+public abstract class RerunnableThread extends Thread {
+    private Boolean flag;
+
+    public RerunnableThread(Boolean flag) {
+        this.flag = flag;
+    }
+    
+    public RerunnableThread() {
+        this(false);
+    }
+
+    public void rerun(Object lockObj) {
+        this.setTrue();
+        synchronized (lockObj) {
+            lockObj.notifyAll();
+        }
+        try {
+            Thread.sleep(100);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    };
+
+    public abstract void rerun();
+
+    public void waitOnce(Object lockObj) {
+        synchronized (lockObj) {
+            while (!this.getFlag()) {
+                try {
+                    lockObj.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    protected void setTrue() {
+        flag = true;
+    }
+
+    protected void setFalse() {
+        flag = false;
+    }
+
+    public Boolean getFlag() {
+        return flag;
+    }
+}
